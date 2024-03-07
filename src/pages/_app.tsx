eaa84eb704ff39a48a3776f2492368f46c166d14
Mariana Rocío Rionda <97262059+MariRionda@users.noreply.github.com>
2024-03-07 19:47:09 -0300

    Header de app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,20 +14,35 @@ export default function App({ Component, pageProps }: AppProps) {
       <I18nextProvider i18n={i18n}>
         <Head>
           <title>VinciBlock</title>
-          <meta name="description" content="VinciBlock exchange" />
+          <meta
+            name="description"
+            content="Tecnología avanzada para un futuro sin límites."
+          />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="keywords"
+            content="VinciBlock, Blockchain, Tecnología blockchain, Cadena de bloques, Bitcoin, Ethereum, Binance, Gnosis Chain, Tron, Solidity, Smart Contract, Desarrollo de software, Desarrollo, Desarrollo de productos tecnológicos, Personalización tecnológica, Software, Auditoría, Auditoría de seguridad, Ciberseguridad, Seguridad digital avanzada, Soluciones digitales, Inteligencia artificial, Reconocimiento facial, Realidad aumentada, Tokenización, Desarrollo de exchanges, Aplicaciones móviles, Innovación tecnológica, Transformación digital, Eficiencia operativa"
+          />
+          <meta property="og:title" content="VinciBlock" />
+          <meta
+            property="og:description"
+            content="Tecnología avanzada para un futuro sin límites."
+          />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/dk2al2urj/image/upload/v1709847641/vinciblock-logo_iremgs.jpg"
+          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className="bigContainer">
           <div className="container">
             <Header />
-            <ToastContainer/>
+            <ToastContainer />
             <Component {...pageProps} />
-            <BackTopButton/>
+            <BackTopButton />
             <Footer />
           </div>
         </main>
-        
       </I18nextProvider>
     </>
   );
